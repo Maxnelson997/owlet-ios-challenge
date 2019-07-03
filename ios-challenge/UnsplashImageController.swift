97ed0b44aaf24b7edabd2b99cb5293c696cb608c
Max <maxnelson997@gmail.com>
2019-07-02 20:12:38 -0600

    created UIImageView  extension to async load images randomly from unsplash

--- ios-challenge/UnsplashImageController.swift
+++ ios-challenge/UnsplashImageController.swift
@@ -9,8 +9,59 @@
 import UIKit
 
 class UnsplashImageController: UIViewController {
+    
+    fileprivate let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    fileprivate let imageStringURL: String = "https://source.unsplash.com/user/maxcodes"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+    }
+    
+    fileprivate func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(imageView)
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        
+        imageView.asyncLoadImage(from: imageStringURL)
+    }
+    
+ 
+}
+
+
+
+extension UIImageView {
+    func asyncLoadImage(from urlString: String) {
+
+        guard let url = URL(string: urlString) else {
+            print("please provide a valid url")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            if let err = err {
+                print("error downloading image", err)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                guard let data = data else { print("no data fetched"); return }
+                let fetchedImage = UIImage(data: data)
+                self.image = fetchedImage
+            }
+        }.resume()
+
     }
 }
--- ios-challenge/ViewController.swift
+++ ios-challenge/ViewController.swift
@@ -12,10 +12,36 @@ class ViewController: UIViewController, StoreSubscriber {
     
     @IBOutlet weak var countLabel: UILabel!
     
+    fileprivate let loadAnImageButton: UIButton = {
+       let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.layer.cornerRadius = 5
+        b.backgroundColor = UIColor.init(red: 5/255, green: 123/255, blue: 252/252, alpha: 1)
+        b.setTitle("Load An Image!", for: .normal)
+        b.setTitleColor(.white, for: .normal)
+        b.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        return b
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         store.subscribe(self)
-        // Do any additional setup after loading the view.
+
+        setupUI()
+    }
+    
+    fileprivate func setupUI() {
+        view.addSubview(loadAnImageButton)
+        loadAnImageButton.widthAnchor.constraint(equalToConstant: 160).isActive = true
+        loadAnImageButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        loadAnImageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loadAnImageButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        loadAnImageButton.addTarget(self, action: #selector(handleLoadAnImage), for: .touchUpInside)
+    }
+    
+    @objc fileprivate func handleLoadAnImage() {
+        let imageController = UnsplashImageController()
+        present(imageController, animated: true)
     }
     
     func newState(state: State) {
